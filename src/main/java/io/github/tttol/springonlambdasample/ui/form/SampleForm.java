53936a63fa32ee36f6ee3c916251dac6aae5ad0e
t-takahashi <tkhstol.929@gmail.com>
2023-04-10 23:09:55 +0900

    implementing validation message logic

--- src/main/java/io/github/tttol/springonlambdasample/ui/form/SampleForm.java
+++ src/main/java/io/github/tttol/springonlambdasample/ui/form/SampleForm.java
@@ -3,6 +3,6 @@ package io.github.tttol.springonlambdasample.ui.form;
 import javax.validation.constraints.*;
 
 public record SampleForm(
-        @Pattern(regexp = "^[a-zA-Z0-9_-]{1,20}$") @NotBlank String userId,
+        @Pattern(regexp = "^[a-zA-Z0-9_-]{1,20}$", message = "{validation.userId.pattern}") @NotBlank String userId,
         @NotBlank String name
 ) { }
